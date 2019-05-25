62ef242e963a2d3a0c317c2b2289afe954bf7687
Pedro Garrido <pe_garrido@outlook.com>
2019-05-25 08:18:42 +0100

    added single compiler input

--- src/utils/compiler-input.ts
+++ src/utils/compiler-input.ts
@@ -1,4 +1,4 @@
-const compilerInput = (sources: any, opts: any) => {
+export const compilerInput = (sources: any, opts: any) => {
     return JSON.stringify({
         language: 'Solidity',
         sources: sources,
@@ -11,10 +11,28 @@ const compilerInput = (sources: any, opts: any) => {
             outputSelection: {
                 '*': {
                     '': ['legacyAST'],
-                    '*': ['abi', 'metadata', 'devdoc', 'userdoc', 'evm.legacyAssembly', 'evm.bytecode', 'evm.deployedBytecode', 'evm.methodIdentifiers', 'evm.gasEstimates']
+                    '*': ['abi', 'metadata', 'devdoc', 'userdoc', 'evm.legacyAssembly', 'evm.bytecode', 'evm.deployedBytecode', 'evm.methodIdentifiers', 'evm.gasEstimates', 'evm.bytecode.sourceMap']
                 }
             }
         }
     })
 }
-export default compilerInput
+
+export const simpleCompilerInput = (sources: any, opts: any) => {
+    return JSON.stringify({
+        language: 'Solidity',
+        sources: sources,
+        settings: {
+            optimizer: {
+                enabled: opts.optimize === true || opts.optimize === 1,
+                runs: 200
+            },
+            libraries: opts.libraries,
+            outputSelection: {
+                '*': {
+                    '*': ['abi', 'evm.bytecode']
+                }
+            }
+        }
+    })
+}
