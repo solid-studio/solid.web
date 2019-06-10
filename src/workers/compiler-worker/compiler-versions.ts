b2433ae16e790f5feecc749954ca2c18e8f8edf4
Pedro Garrido <pe_garrido@outlook.com>
2019-06-10 06:23:24 +0100

    moved compiler worker to feature

--- src/workers/compiler-worker/compiler-versions.ts
+++
@@ -1,5 +0,0 @@
-export const solidityCompilerVersionsMap = new Map()
-
-solidityCompilerVersionsMap.set('0.5.8', 'soljson-v0.5.8+commit.23d335f2.js')
-
-export const solidityCompilerInstanceMap = new Map()
--- src/workers/compiler-worker/index.ts
+++
@@ -1,4 +0,0 @@
-// eslint-disable-next-line import/no-webpack-loader-syntax
-import MyWorker from 'worker-loader!./worker'
-
-export default MyWorker
