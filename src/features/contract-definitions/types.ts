a7879482901ab4f30164d70c0927e25b217aea1e
Pedro Garrido <pe_garrido@outlook.com>
2019-09-13 18:57:21 +0100

    fix fakers

--- src/features/contract-definitions/types.ts
+++ src/features/contract-definitions/types.ts
@@ -1,7 +1,9 @@
+import { AbiItem } from "web3-utils";
+
 export interface ContractDefinition {
     _id?: string
     name: string
     sourceCode: string
-    abi: []
+    abi: AbiItem[]
     bytecode: string
 }
