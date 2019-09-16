d68132086fcaf452d53a5d351b2b28caa4f502e0
Pedro Garrido <pe_garrido@outlook.com>
2019-09-16 06:13:52 +0100

    cleaned main content view

--- src/views/MainContentView.tsx
+++ src/views/MainContentView.tsx
@@ -1,175 +1,71 @@
 import React from 'react'
-
-import { Collapse, Icon, Tabs } from 'antd'
-import copy from 'copy-to-clipboard'
-import MonacoEditor from 'react-monaco-editor'
 import { connect } from 'react-redux'
-import { AbiItem } from 'web3-utils'
+import { bindActionCreators, Dispatch, ActionCreator, Action } from 'redux'
+import { Tabs } from 'antd';
 
-import { ApplicationState } from '../features/rootReducer'
-import { Contract } from '../features/contracts' // TODO: FIX, contract should in contracts
-import { ContractActions } from '../features/contracts/ContractActions'
-import { SolidTerminal } from '../features/terminal/SolidTerminal'// TODO: fix import
+import { ApplicationState } from 'features/rootReducer'
+import { Tab, setTabActiveById } from 'features/tabs'
 
-import { Wrapper, Editor, Results, Details, TableDetails, CollapseStyled } from "./components";
-import { SAMPLE_ABI, SAMPLE_CONTRACT } from "./sample-data"
+import { EditorView } from './EditorView'
+import { ContractsView } from './ContractsView'
+import { TransactionsView } from './TransactionsView'
 
 const TabPane = Tabs.TabPane
-const Panel = Collapse.Panel
 
-interface Props {
-    selectedContract: Contract | undefined
+interface OwnProps {
+    tabs: Tab[]
+    activeTab: Tab
 }
 
-interface State {
-    code: string
-    editor: any
+interface DispatchProps {
+    setTabActiveById: ActionCreator<Action>
 }
 
-const options = {
-    selectOnLineNumbers: true,
-    minimap: {
-        enabled: false
-    },
-    scrollbar: {
-        vertical: 'hidden' as any
-    },
-    fixedOverflowWidgets: false,
-    scrollBeyondLastLine: false,
-    height: '100%',
-    width: '100%'
-}
+type AllProps = OwnProps & DispatchProps
 
-export class MainContentView extends React.Component<Props, State> {
-    private editor = null
-    private child: {
-        console?: Console
-    } = {}
-
-    constructor(props: Props) {
-        super(props)
-        this.state = {
-            editor: undefined,
-            code: SAMPLE_CONTRACT
-        }
-    }
-
-    editorDidMount = (editor: any, monaco: any) => {
-        console.log('editorDidMount', editor)
-        this.editor = editor
-        editor.focus()
-    }
-
-    handleResize = () => {
-        if (this.editor) {
-            ; (this.editor as any).layout()
-        }
-    }
-
-    componentDidMount() {
-        window.addEventListener('resize', this.handleResize)
-    }
-
-    onChange(newValue: any, e: any) {
-        console.log('onChange', newValue, e)
-    }
-    callback(key: any) {
-        console.log(key)
-    }
-
-    copyByteCode = (byteCode: string) => {
-        console.log('copyByteCode clicked')
-        copy(byteCode)
-    }
-
-    copyABI = (abi: AbiItem[]) => {
-        console.log('copyABI clicked')
-        copy(JSON.stringify(abi))
-    }
-
-    echo = (text: string) => {
-        ; (this.child as any).console.log(text)
-    }
-
-    promptLabel = () => {
-        return '> '
-    }
+export class MainContentView extends React.Component<AllProps> {
     render() {
-        const { selectedContract } = this.props
+        const { tabs, activeTab } = this.props
+        console.log('ACTIVE TAB', activeTab)
+        console.log("TABS", tabs)
         return (
-            <Wrapper data-testid="home-view">
-                <Editor>
-                    <Tabs type="card" style={{ paddingLeft: '1em', paddingRight: '1em', height: '100%' }}>
-                        <TabPane tab={selectedContract && selectedContract.name} key="1" style={{ height: '100%' }}>
-                            <MonacoEditor
-                                language="solidity"
-                                theme="vs-dark"
-                                value={selectedContract && selectedContract.sourceCode}
-                                options={options}
-                                onChange={this.onChange}
-                                editorDidMount={this.editorDidMount}
-                            />
-                        </TabPane>
-                    </Tabs>
-                </Editor>
-                <Results>
-                    <SolidTerminal />
-                </Results>
-                <Details>
-                    {selectedContract && (
-                        <TableDetails>
-                            <thead>
-                                <tr>
-                                    <th>Details</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>{selectedContract.name}</td>
-                                </tr>
-                                <tr>
-                                    <td>Address</td>
-                                    <td>{selectedContract.address}</td>
-                                </tr>
-                                <tr>
-                                    <td>ABI</td>
-                                    <td>
-                                        <Icon onClick={() => this.copyABI(selectedContract.abi)} type="copy" />
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>Bytecode</td>
-                                    <td>
-                                        <Icon onClick={() => this.copyByteCode(selectedContract.bytecode)} type="copy" />
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>Size </td>
-                                    <td>12 KB</td>
-                                </tr>
-                            </tbody>
-                        </TableDetails>
-                    )}
-                    <CollapseStyled defaultActiveKey={['0']} onChange={this.callback} bordered={false}>
-                        {selectedContract && (
-                            <Panel header="Methods" key="2">
-                                <ContractActions abi={SAMPLE_ABI || selectedContract.abi} />
-                            </Panel>
-                        )}
-                    </CollapseStyled>
-                </Details>
-            </Wrapper>
+            <div style={{ height: "100%" }} data-testid="main-content-view">
+                {tabs && tabs.length > 0 &&
+                    <Tabs type="card" style={{ paddingLeft: '1em', paddingRight: '1em', height: '100%' }}
+                        activeKey={activeTab && activeTab.id}
+                        onTabClick={(e: Tab) => {
+                            this.props.setTabActiveById(e)
+                        }}>
+                        {tabs.map((currentTab: Tab) => {
+                            return <TabPane tab={`${currentTab.title}`} key={currentTab.id} className={'full-tab'}>
+                                {currentTab.type === 'transactions' && <TransactionsView />}
+                                {currentTab.type === 'contracts' && <ContractsView />}
+                                {currentTab.type === 'editor' && <EditorView selectedContract={currentTab.data} />}
+                            </TabPane>
+                        })}
+                    </Tabs>}
+            </div>
         )
     }
 }
 
 const mapStateToProps = (state: ApplicationState) => {
     return {
-        selectedContract: state.contractState.currentContract
+        tabs: state.tabsManagerState.tabs,
+        activeTab: state.tabsManagerState.activeTab
     }
 }
 
-export default connect(
+const mapDispatchToProps = (dispatch: Dispatch) => {
+    return bindActionCreators(
+        {
+            setTabActiveById
+        },
+        dispatch
+    )
+}
+
+export default connect<{}, DispatchProps, {}, ApplicationState>(
     mapStateToProps,
-    null
+    mapDispatchToProps
 )(MainContentView)
