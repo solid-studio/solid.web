e3ed5ac18cbdc48cd537ef9d60a92f1d40867a2c
Rick <rick_vazquez@outlook.com>
2019-10-07 15:22:54 +0900

    space

--- src/features/blocks/reducer.test.ts
+++ src/features/blocks/reducer.test.ts
@@ -1,7 +1,9 @@
 import { appReducer, initialState } from './reducer'
 import { blocksReceived } from './actions'
 import { buildFakeBlocks } from '@solidstudio/solid.types'
+
 describe('Blocks reducer', () => {
+
     test('ActionType.BLOCKS_RECEIVED', () => {
         const blocks = buildFakeBlocks()
         const blocksReceivedAction = blocksReceived(blocks);
@@ -11,4 +13,5 @@ describe('Blocks reducer', () => {
         expect(newState.currentBlock).toEqual(blocks[0]) // TODO: Am I sure the current block has to be the first one?, check reducer too..
         expect(newState.getBlocksStatus).toEqual(initialState.getBlocksStatus)
     })
+
 })
