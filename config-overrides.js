9dd4398505feaca63bea00024bb48b01d07c93e6
Solid Studio <solidstudio@labchain.io>
2019-10-18 05:48:03 +0100

    trying to overwrite dev server

--- config-overrides.js
+++ config-overrides.js
@@ -1,4 +1,21 @@
-const { override, fixBabelImports, addLessLoader } = require('customize-cra');
+const { override, overrideDevServer, fixBabelImports, addLessLoader } = require('customize-cra');
+const fs = require('fs');
+const path = require('path')
+
+const notifyWatchers = () => config => {
+    console.log("Notify watchers called", config)
+    // config.writeToDisk = true;
+    // config.contentBase = './dist' // currently in public
+    config.after = (app, server) => {
+        fs.writeFile("./log-after.json", config.writeToDisk, function (err) {
+            if (err) {
+                return console.log(err);
+            }
+            console.log("The file was saved!");
+        });
+    }
+    return config;
+}
 
 const addWorkerLoaderModule = () => config => {
     config.module.rules.push({
@@ -8,20 +25,33 @@ const addWorkerLoaderModule = () => config => {
         }
     })
     config.output.globalObject = 'this';
+    // config.output = { path: path.resolve(__dirname, '..', 'disttest') }
+    // config.output = {
+    //     filename: '[name].bundle.js',
+    //     path: path.resolve(__dirname, 'dist'),
+    //     publicPath: '/',
+    // }
     return config;
 }
 
-module.exports = override(
-    addWorkerLoaderModule(),
-    fixBabelImports('import', {
-        libraryName: 'antd',
-        libraryDirectory: 'es',
-        style: 'true',
-    }),
-    addLessLoader({
-        javascriptEnabled: true
-    })
-)
+
+module.exports = {
+    webpack: override(
+        addWorkerLoaderModule(),
+        fixBabelImports('import', {
+            libraryName: 'antd',
+            libraryDirectory: 'es',
+            style: 'true',
+        }),
+        addLessLoader({
+            javascriptEnabled: true
+        })
+    ),
+    devServer: overrideDevServer(
+        // dev server plugin
+        notifyWatchers()
+    )
+};
 
 // module.exports = {
 //     webpack: rewireWebpackConfig,
