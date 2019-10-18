58d37878163b7b97088c682c64f9472cf6c1c98f
Solid Studio <solidstudio@labchain.io>
2019-10-18 06:11:36 +0100

    removed folder

---
+++ eject/my-app/dist/main.ca0c3fe42dfa7e8cfd4e.hot-update.js
@@ -0,0 +1,36 @@
+webpackHotUpdate("main",{
+
+/***/ "./node_modules/css-loader/dist/cjs.js?!./node_modules/postcss-loader/src/index.js?!./src/App.css":
+false,
+
+/***/ "./node_modules/css-loader/dist/cjs.js?!./node_modules/postcss-loader/src/index.js?!./src/index.css":
+false,
+
+/***/ "./src/App.css":
+false,
+
+/***/ "./src/App.tsx":
+false,
+
+/***/ "./src/index.css":
+false,
+
+/***/ "./src/index.tsx":
+/*!***********************!*\
+  !*** ./src/index.tsx ***!
+  \***********************/
+/*! no exports provided */
+/***/ (function(module, exports) {
+
+throw new Error("Module build failed (from ./node_modules/eslint-loader/dist/cjs.js):\nError: ENOENT: no such file or directory, open '/Users/edsonalcala/Projects/solid-launch-december-2019/solid.web/eject/my-app/src/index.tsx'");
+
+/***/ }),
+
+/***/ "./src/logo.svg":
+false,
+
+/***/ "./src/serviceWorker.ts":
+false
+
+})
+//# sourceMappingURL=main.ca0c3fe42dfa7e8cfd4e.hot-update.js.map
