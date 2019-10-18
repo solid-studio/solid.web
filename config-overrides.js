2bb259185e798ea30944b5ebb82c1df5bc00104f
Solid Studio <solidstudio@labchain.io>
2019-10-18 06:11:03 +0100

    using patch

--- config-overrides.js
+++ config-overrides.js
@@ -1,21 +1,4 @@
-const { override, overrideDevServer, fixBabelImports, addLessLoader } = require('customize-cra');
-const fs = require('fs');
-const path = require('path')
-
-const notifyWatchers = () => config => {
-    console.log("Notify watchers called", config)
-    // config.writeToDisk = true;
-    // config.contentBase = './dist' // currently in public
-    config.after = (app, server) => {
-        fs.writeFile("./log-after.json", config.writeToDisk, function (err) {
-            if (err) {
-                return console.log(err);
-            }
-            console.log("The file was saved!");
-        });
-    }
-    return config;
-}
+const { override, fixBabelImports, addLessLoader } = require('customize-cra');
 
 const addWorkerLoaderModule = () => config => {
     config.module.rules.push({
@@ -25,15 +8,8 @@ const addWorkerLoaderModule = () => config => {
         }
     })
     config.output.globalObject = 'this';
-    // config.output = { path: path.resolve(__dirname, '..', 'disttest') }
-    // config.output = {
-    //     filename: '[name].bundle.js',
-    //     path: path.resolve(__dirname, 'dist'),
-    //     publicPath: '/',
-    // }
     return config;
 }
-
 
 module.exports = {
     webpack: override(
@@ -46,19 +22,5 @@ module.exports = {
         addLessLoader({
             javascriptEnabled: true
         })
-    ),
-    devServer: overrideDevServer(
-        // dev server plugin
-        notifyWatchers()
     )
 };
-
-// module.exports = {
-//     webpack: rewireWebpackConfig,
-//     jest: (config) => {
-//         config.moduleNameMapper = {
-//             "\\.worker.js": "<rootDir>/__mocks__/workerMock.js"
-//         };
-//         return config;
-//     }
-// };
