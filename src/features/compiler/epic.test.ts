c08a05bcd854b76b402d1f34e5f6ca83a39f8958
Solid Studio <solidstudio@labchain.io>
2019-11-27 13:49:56 +0000

    removed unused code in compiler epic

--- src/features/compiler/epic.test.ts
+++ src/features/compiler/epic.test.ts
@@ -1,39 +1,17 @@
 import { ActionsObservable, StateObservable } from 'redux-observable'
-import { Subject, of } from 'rxjs'
+import { Subject } from 'rxjs'
 
 import { ApplicationState, initialState } from '../rootReducer'
 
 import {
-  LoadCompilerVersionAction,
-  LoadCompilerVersionResultAction,
-  SetupMessageDispatcherAction
+  LoadCompilerVersionAction
 } from './action-types'
 import {
-  loadCompilerVersion,
-  compilerVersionLoaded,
-  loadCompilerVersionFailed,
-  setupMessageDispatcher
+  loadCompilerVersion
 } from './actions'
-import { setupMessageDispatcherEpic, loadCompilerVersionEpic, loadCompilerVersionResultEpic } from './epic'
+import { loadCompilerVersionEpic } from './epic'
 
 describe('Compiler Epic Tests', () => {
-  test('setupMessageDispatcherEpic', done => {
-    const action: SetupMessageDispatcherAction = setupMessageDispatcher()
-
-    const actions$ = ActionsObservable.of(action)
-
-    const applicationState$: StateObservable<ApplicationState> = new StateObservable<ApplicationState>(
-      new Subject(),
-      initialState
-    )
-
-    const output$ = setupMessageDispatcherEpic(actions$, applicationState$)
-
-    output$.subscribe(action => {
-      // expect(action).toEqual() // TODO
-      done()
-    })
-  })
 
   test('loadCompilerVersionEpic', done => {
     const action: LoadCompilerVersionAction = loadCompilerVersion()
@@ -49,44 +27,6 @@ describe('Compiler Epic Tests', () => {
 
     output$.subscribe(action => {
       // If I'm not returning nothing, should I spy? Use mocks in the worker...
-      // expect(action).toEqual() // TODO
-      done()
-    })
-  })
-
-  // I realise I don't need this tests and the epic..
-  test.skip('loadCompilerVersionResultEpic when compiler version is loaded', done => {
-    const action: LoadCompilerVersionResultAction = compilerVersionLoaded()
-
-    const actions$ = ActionsObservable.of(action)
-
-    const applicationState$: StateObservable<ApplicationState> = new StateObservable<ApplicationState>(
-      new Subject(),
-      initialState
-    )
-
-    const output$ = setupMessageDispatcherEpic(actions$, applicationState$)
-
-    output$.subscribe(action => {
-      // expect(action).toEqual() // TODO
-      done()
-    })
-  })
-
-  // TODO: To finish
-  test.skip('loadCompilerVersionResultEpic when compiler version fail', done => {
-    const action: LoadCompilerVersionResultAction = loadCompilerVersionFailed()
-
-    const actions$ = ActionsObservable.of(action)
-
-    const applicationState$: StateObservable<ApplicationState> = new StateObservable<ApplicationState>(
-      new Subject(),
-      initialState
-    )
-
-    const output$ = setupMessageDispatcherEpic(actions$, applicationState$)
-
-    output$.subscribe(action => {
       // expect(action).toEqual() // TODO
       done()
     })
